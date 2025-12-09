337a3bfe874ad66261e014879b592758fd2a8e70
ramirezfer772 <ramirezfer772@gmail.com>
2025-12-08 19:48:38 -0600

    add 496. Next Greater Element I

---
+++ 496. Next Greater Element I.js
@@ -0,0 +1,30 @@
+// 496. Next Greater Element I
+// https://leetcode.com/problems/next-greater-element-i/description/
+
+var nextGreaterElement = function (nums1, nums2) {
+  const stack = [];
+  const map = {};
+
+  for (let n of nums2) {
+    while (stack.length && n > stack[stack.length - 1]) {
+      map[stack.pop()] = n;
+    }
+    stack.push(n);
+    console.log(stack, map);
+  }
+
+  console.log(stack, map);
+
+  for (let n of stack) {
+    map[n] = -1;
+  }
+
+  console.log("map:", map);
+
+  return nums1.map((n) => map[n]);
+};
+
+// nextGreaterElement([4, 1, 2], [1, 3, 4, 2]);
+nextGreaterElement([4, 1, 2], [2, 1, 4]);
+
+// [-1, 3, -1]
